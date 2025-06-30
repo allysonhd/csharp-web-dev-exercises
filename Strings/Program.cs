f31b1076eda856a09601f74501bda404a1e07ff3
Allysonhd <166686180+allysonhd@users.noreply.github.com>
2025-06-30 14:35:42 -0500

    completed string method practice

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -4,3 +4,13 @@
 
 Console.WriteLine("Hello, Strings!");
 
+string text = @"I would not, could not, in a box. I would not, could not with a fox.
+I will not eat them in a house. I will not eat them with a mouse.";
+
+// Console.WriteLine(text);
+
+string[] spaceArray = text.Split(" ");
+// Console.WriteLine(string.Join(",", spaceArray));
+
+string[] sentenceArray = text.Split(".");
+Console.WriteLine(string.Join(";", sentenceArray));
