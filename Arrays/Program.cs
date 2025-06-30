a56178d163cca8eca2d0c6e3ce74abcc3d23f09f
Allysonhd <166686180+allysonhd@users.noreply.github.com>
2025-06-30 14:07:16 -0500

    completed loop through array exercise

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,5 +2,16 @@
 
 //TODO: Write your code for Array Practice below:
 
+int [] numbersArray = {1, 1, 2, 3, 5, 8};
+for (int i = 0; i < numbersArray.Length; i++)
+{
+    Console.WriteLine(numbersArray[i]);
+}
+
+for (int i = 0; i < numbersArray.Length; i++)
+{
+    if (i%2 != 0) {Console.WriteLine(i);}
+}
+
 Console.WriteLine("Hello, Arrays!");
 
